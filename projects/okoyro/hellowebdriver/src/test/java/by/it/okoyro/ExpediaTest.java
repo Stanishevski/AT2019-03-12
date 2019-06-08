a47e91454738c5743659c3a6ef7bcb32c4586bd9
okoyro <oksana.koyro@gmail.com>
2019-06-08 13:01:49 +0300

    at_28 taskB and chromeDriver headless settings

--- projects/okoyro/hellowebdriver/src/test/java/by/it/okoyro/ExpediaTest.java
+++ projects/okoyro/hellowebdriver/src/test/java/by/it/okoyro/ExpediaTest.java
@@ -1,9 +1,12 @@
 package by.it.okoyro;
 
 import by.it.okoyro.pages.HomePage;
+import by.it.okoyro.pages.HomePageFactory;
 import by.it.okoyro.pages.SearchResultPage;
+import by.it.okoyro.pages.SearchResultPageFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,14 +19,16 @@ public class ExpediaTest {
 
 	@BeforeMethod
 	public void setUp() {
-		System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
-		driver = new ChromeDriver();
+		//		System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setHeadless(true);
+		driver = new ChromeDriver(chromeOptions);
 	}
 
-	@Test(invocationCount = 5)
+	@Test
 	public void taskA() throws Exception {
 		driver.get("https://www.expedia.com.my/");
-		
+
 		HomePage homePage = new HomePage(driver);
 		SearchResultPage resultPage = homePage
 				.selectSearchFlightMode()
@@ -36,9 +41,23 @@ public class ExpediaTest {
 		Assert.assertTrue(searchResultsQuantity > 1);
 	}
 
+	@Test
+	public void taskB() {
+		driver.get("https://www.expedia.com.my/");
+
+		HomePageFactory homePageFactory = new HomePageFactory(driver);
+		SearchResultPageFactory resultPageFactory = homePageFactory
+				.selectSearchFlightMode()
+				.setFlightFrom("minsk")
+				.setFlightTo("moscow")
+				.setDepartureDate("20/06/2019")
+				.setReturnDate("05/07/2019")
+				.performSearch();
+	}
+
 	@AfterMethod
-		public void tearDown() {
-			driver.quit();
-		}
+	public void tearDown() {
+		driver.quit();
+	}
 
 }
